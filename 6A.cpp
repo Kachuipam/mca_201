3b50991a029b53a2c91af73d87830c5fa12d711e
Kachuipam <31645361+Kachuipam@users.noreply.github.com>
2018-03-28 12:14:17 +0530

    Create a class for a Node for Single Linked list.

--- 6A.cpp
+++ 6A.cpp
@@ -21,19 +21,84 @@ class SLinkedList {
 	output value: none
 	side effects: Class SlinkedList defines Single Linked LIst class
 	approach: Class definition
-	*/						
-	public:
-	SLinkedList();							// empty list constructor
-	~SLinkedList();							// destructor
-	bool empty() const; 					// is list empty?
-	const string& front() const; 			// get front element
-	void addFront(const string& e); 		// add to front of list
-	void addBack(const string& e); 			// add to back of list
-	void removeFront();			 			// remove from front 
-	void removeEnd();						// remove from end
-	void print(); 							// prints the SLL
+	*/	
+	
 	private:
-	SNode* head; 							// pointer to the head of list
+  	    SNode* head; 							// pointer to the head of list					
+	public:
+		SLinkedList()							// empty list constructor
+		{
+			head=NULL;	
+		};
+		~SLinkedList()							// destructor
+		{
+			delete head;
+		};
+		bool empty() const; 					// is list empty?
+		{
+	        if(head==NULL)
+			  	return true;
+			else 
+		        return false;	
+		};
+		const string& front() const 			// get front element
+		{
+		 	if(head==NULL)
+	 		{
+		  	    return -1;
+	 		}	
+	 		else
+	 		{
+	 			return head->elem;
+	 		}
+		};
+		void addFront(const string& e); 		// add to front of list
+		{
+			SNode newNode = new SNode;
+			newNode->elem=e;
+			newNode->next=head;
+			head=newNode;
+		}
+		void addBack(const string& e); 			// add to back of list
+		{
+			SNode temp = new SNode;
+			Snode newNode = new SNode;
+			newNode->elem=e;
+			newNode->next=NULL;
+			temp=head;
+			while(temp->next!=NULL)
+			{
+				temp=temp->next;
+				temp->next=newNode;
+			}
+		};
+		void removeFront();			 			// remove from front
+		{
+			SNode temp = new SNode; 
+			if(head==NULL)
+			   return;
+            if(head!=NULL&&next==NULL)
+				  cout<<"Element removed is "<<head;
+				  head==NULL;
+ 		    else
+		 	{
+		 		cout<<"Element removed is "<<head->elem;
+			 	head = head->next;
+			}
+ 		}; 
+		void removeEnd();						// remove from end
+		{
+			SNode newNode = new SNode;
+			temp=head;
+			if(head==NULL)
+		        return;
+			if(head!=NULL&&next=NULL)
+		 	    head==NULL;
+			while(temp->next!=NULL)
+			{
+				temp=temp->next;
+			}
+			temp->next=NULL;
+		};
+		void print(); 							// prints the SLL
 };
-
-
