e6f0cc18688e0d0e1eba7681e7d16a731067e8b1
Kachuipam <31645361+Kachuipam@users.noreply.github.com>
2018-04-18 22:05:02 +0530

    Templated Singly Linked List

--- 6B.cpp
+++ 6B.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
-#include<string>
 using namespace std;
-template <class T> class SLinkedList;
-template <class T>
-class SNode { 
+#include<cstring>
+template<class T> class SLinkedList ;
+template<class T>
+class SNode 
+{ 
 	/*   			
 	objective: Create a class for a Node for Single Linked list
 	input parameters: none
@@ -15,184 +16,178 @@ class SNode {
 	private:
 	  T elem; 
 	  SNode<T>* next; 
-	friend class SLinkedList<T>; // provides SLinkedList access to SNode
+	  friend class SLinkedList<T>; // provides SLinkedList access to SNode
 };
 
-template <class T>
-class SLinkedList { 
+template<class T>
+class SLinkedList 
+{ 
 	/*   			
 	objective: Create a Single LInked List class 
 	input parameters: none
 	output value: none
 	side effects: Class SlinkedList defines Single Linked LIst class
 	approach: Class definition
-	*/	
-	
-	private:
-  	    SNode<T>* head; 							// pointer to the head of list					
+	*/						
 	public:
-		SLinkedList()							// empty list constructor
-		{
-			head=NULL;	
-		};
-		~SLinkedList()							// destructor
-		{
-			delete head;
-		};
-		bool empty() const 					// is list empty?
-		{
-	        if(head==NULL)
-			  	return true;
-			else 
-		        return false;	
-		};
-		const T& front() const;
-		void addBack(const T& e);
-		void addFront(const T& e);
-		void removeFront();
-		void removeBack();
-		void print();
+	SLinkedList()
+	{
+		head=NULL;
+	}							// empty list constructor
+	~SLinkedList()
+	{
+		delete head;
+	}							// destructor
+	bool empty() const; 					// is list empty?
+	const T& front() const; 			// get front element
+	void addFront(const T& e); 		// add to front of list
+	void addBack(const T& e); 			// add to back of list
+	void removeFront();			 			// remove from front 
+	void removeEnd();						// remove from end
+	void print(); 							// prints the SLL
+	SNode <T> *newNode(const T& e)
+	{
+		SNode<T> *temp=new SNode<T>();
+		temp->next=NULL;
+		temp->elem=e;
+		return temp;
+	}
+	private:
+	SNode<T> *head; 							// pointer to the head of list
 };
-template <class T>
-const T & SLinkedList <T> :: front() const 			// get front element
-		{
-		 	if(head==NULL)
-	 		{
-		  	    return NULL;
-			}	
-			else
-	 		{
-				return head->elem;
-			}
-};
-template <class T>
-void SlinkedList <T>::addFront(const T & e) 		// add to front of list
+template<class T>
+bool SLinkedList<T>::empty() const
 {
-			SNode<T> *newNode = new SNode;
-			newNode->elem=e;
-			newNode->next=head;
-			head=newNode;
-};
-template <class T>		
-void SlinkedList <T> :: addBack(const T& e) 			// add to back of list
-		{
-			SNode<T> *temp = new SNode<T>;
-			SNode<T> *newNode = new SNode<T>;
-			newNode->elem=e;
-			newNode->next=NULL;
-			if(head==NULL)
-			{
-				head=newNode;
-			}
-			else
-			{
-				temp=head;
-				while(temp->next!=NULL)
-				{
-					temp=temp->next;
-					temp->next=newNode;
-				}
-			}
-		};
-template <class T>		
-void SlinkedList<T>::removeFront()			 			// remove from front
+	return head==NULL;
+}
+template<class T>
+const T& SLinkedList<T>::front() const
 {
-			SNode<T> *temp = new SNode<T>; 
-			if(head==NULL)
-			   return;
-            else if(head->next==NULL)
-            {
-				  cout<<"Element removed is "<<head->elem;
-				  head==NULL;
-			}
- 		    else
-		 	{
-		 		cout<<"Element removed is "<<head->elem<<"\n";
-			 	head = head->next;
-			}
-}; 
-template <class T>		
-void SlinkedList<T>::removeBack()						// remove from end
-		{
-			SNode<T> *temp = new SNode<T>;
-			temp=head;
-			if(head==NULL)
-		        return;
-			if(head->next=NULL)
-		 	    head==NULL;
-			while(temp->next!=NULL)
-			{
-				temp=temp->next;
-			}
-			temp->next=NULL;
-		};
-template <class T>
-void SlinkedList<T>::print() 							// prints the SLL
-		{
-			SNode<T> *temp = new SNode<T>;
-			temp=head;
-			if(head==NULL)
-	            cout<<"Empty list \n";
-			if(head->next==NULL)
-			    cout<<"Element in the list is  "<<head->elem<<"\n";
-			while(temp->next!=NULL)
-			{
-				cout<<"Elements in the list are "<<temp->elem;
-				temp=temp->next;
-			}
-			if (temp!=NULL)
-   				cout<<" "<<temp->elem<<"\n";
-		}
-};
+	return head->elem;
+}
+template<class T>
+void SLinkedList<T>::addFront(const T& e)
+{
+	SNode<T> *node =newNode(e);
+	if(head==NULL)
+	{
+		head=node;
+	}
+	else
+	{
+		node->next=head;
+		head=node;
+	}
+	
+}
+template<class T>
+void SLinkedList<T>::addBack(const T& e)
+{
+	SNode<T> *node =newNode(e);
+    if (head==NULL) {
+        head=node;
+    }
+    else {
+        SNode<T> *temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next; 
+        }
+        temp->next = node;
+    }
+    
+}
+template<class T>
+void SLinkedList<T>::removeFront()
+{
+	if(head->next==NULL)
+	{
+		head=NULL;
+	}
+	else if(!empty())
+	{
+		head=head->next;
+	}
 
+}
+template<class T>
+void SLinkedList<T>::removeEnd()
+{
+	SNode<T> *temp = head;
+	if(head->next==NULL)
+	{
+		head=NULL;
+	}
+	else
+	{
+		while(temp->next!=NULL)
+		{
+			temp=temp->next;
+		}
+	}
+	delete temp;
+}
+template<class T>
+void SLinkedList<T>::print()
+{
+	SNode<T> *temp=head;
+	cout<<"\n Linked list is: ";
+	while(temp!=NULL)
+	{
+		cout<<temp->elem<<" ";
+		temp=temp->next;
+	}
+}
 int main()
 {
-	SLinkedList <string>sll;
-	string elem;
-	char ch;
-	do
-	{
-		cout<<"-----------------MENU-----------------\n";
-		cout<<"1.Add Front\n";
-		cout<<"2.Add Back\n";
-		cout<<"3.Remove Front\n";
-		cout<<"4.Remove Back\n";
-		cout<<"5.Print List\n";
-		cout<<"6.Print Front\n";
-		cout<<"----------------Enter E to exit----------------\n";
-		cout<<"-->>\t";
-		cin>>ch;
-		switch(ch)
-		{
-			case '1' :
-				cout<<"Enter element to add\t\n";
-				cin>>elem;
-				sll.addFront(elem);
-				break;
-			case '2' :
-				cout<<"Enter element to add in back\t\n";
-				cin>>elem;
-				sll.addBack(elem);
-				break;
-			case '3' :
-				sll.removeFront();
-				break;
-			case '4' :
-				sll.removeBack();
-				break;
-			case '5' :
-				sll.print();
-				break;
-			case '6' :
-				cout<<"Front element is \t"<<sll.front()<<"\n";
-				break;
-			default :
-				if(ch!='e')
-				{
-					cout<<"Invalid Option! \n";
-				}
-				break;
-		};
-	}
-	while(ch!='e');
-} 
+
+	SLinkedList<int> sll;
+    while(1) {
+        cout << "\n1.Insert at front\n2.Insert at end\n3.Delete at begin\n4.Delete at end\n5.Display\n6.Exit\n";
+        int choice;
+        cout << "\nEnter Your Choice\t:\t";
+        cin >> choice ;
+        int temp;
+        switch(choice) {
+            case 1 :    cout << "\nEnter New Element\t:\t";
+                        cin>>temp;
+                        sll.addFront(temp);
+                        break;
+            case 2 :    cout << "\nEnter New Element\t:\t";
+                       cin>>temp;
+                        sll.addBack(temp);
+                        break;
+            case 3 :    if(sll.empty())
+            			{				
+                            cout << "\nLinked List is Empty.\n";
+                        }
+                        else 
+                        {
+                            sll.removeFront();
+                        }
+                        break;
+            case 4 :    if(sll.empty())
+            			{				
+                            cout << "\nLinked List is Empty.\n";
+                        }
+                        else 
+                        {
+                            sll.removeEnd();
+                        }
+                        break;
+            case 5:		if(sll.empty())
+            			{
+            				cout << "\nLinked List is Empty.\n";
+            			}
+            			else
+            			{
+            				sll.print();
+            			}
+            			break;
+            case 6 :    exit(0);
+                        	break;
+            default:    cout << "\nInvalid Input\n";
+            				break;
+        }
+    }
+    return 0;
+}
